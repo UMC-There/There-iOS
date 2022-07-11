5b1ee565dc1360a7832c3ff3b4fa211a17ceef99
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2022-07-11 14:38:15 +0900

    Initial Commit

---
+++ There-iOS/There_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  There_iOSApp.swift
+//  There-iOS
+//
+//  Created by 양채연 on 2022/07/11.
+//
+
+import SwiftUI
+
+@main
+struct There_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
