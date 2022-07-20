c42b748cfc78c5c39e98e3d19e518f3ef7239349
mal0070 <mal0070@naver.com>
2022-07-20 16:16:18 +0900

    Feat: add MypageCell

---
+++ There-iOS/MyPageCell/PortfolioCollectionViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  PortfolioCollectionViewCell.swift
+//  There-iOS
+//
+//  Created by 이민아 on 2022/07/20.
+//
+
+import UIKit
+
+class PortfolioCollectionViewCell: UICollectionViewCell {
+    static let identifier = "PortfolioCollectionViewCell"
+    
+    @IBOutlet weak var portImageView: UIImageView!
+    @IBOutlet weak var portTitleLabel: UILabel!
+    @IBOutlet weak var portCountLabel: UILabel!
+    @IBOutlet weak var viewerCountLabel: UILabel!
+    @IBOutlet weak var likeCountLabel: UILabel!
+    @IBOutlet weak var commentCountLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
