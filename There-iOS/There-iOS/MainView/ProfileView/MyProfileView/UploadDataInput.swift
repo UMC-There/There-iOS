d7c1468cbc3a5d7fed9e4d94e4d56b506ec05767
mal0070 <mal0070@naver.com>
2022-08-11 15:23:52 +0900

    Fix: nari2 update

---
+++ There-iOS/There-iOS/MainView/ProfileView/MyProfileView/UploadDataInput.swift
@@ -0,0 +1,15 @@
+//
+//  UploadDataInput.swift
+//  There-iOS
+//
+//  Created by 이민아 on 2022/08/11.
+//
+
+
+import Foundation
+
+struct UploadDataInput: Codable {
+    var images : [String?]
+    var jsonList : String
+    var userIdx : Int32
+}
