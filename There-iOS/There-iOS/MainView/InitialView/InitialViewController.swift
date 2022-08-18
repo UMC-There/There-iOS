655ca036ee57dd236b36dc256d4ed7082c34ec38
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2022-08-18 19:23:43 +0900

    Design: Fix Initial Design 💄

--- There-iOS/There-iOS/MainView/InitialView/InitialViewController.swift
+++ There-iOS/There-iOS/MainView/InitialView/InitialViewController.swift
@@ -16,11 +16,41 @@ class InitialViewController: UIViewController {
     convenience init(bgColor: UIColor) {
         self.init()
         self.view.backgroundColor = bgColor
-    title = "로그인"
     }
     
     // MARK: - Property
+    
+    private lazy var appTitle: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 35.0, weight: .bold)
+        label.numberOfLines = 2
+        label.textColor = .label
+        label.text = "세상 어디에도 없는 곳,"
+        
+        return label
 
+    }()
+    
+    private lazy var appTitle2: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 35.0, weight: .bold)
+        label.numberOfLines = 2
+        label.textColor = .label
+        label.text = "그곳"
+        
+        return label
+
+    }()
+
+
+    private lazy var appLogo: UIImageView = {
+       let appLogo = UIImageView()
+        appLogo.image = UIImage(named: "appLogo")
+        appLogo.layer.cornerRadius = CGFloat(10)
+        appLogo.clipsToBounds = true
+        
+        return appLogo
+    }()
     
     private let loginBtn = CustomButton(text: "로그인", bgColor: UIColor.black, titleColor: UIColor.white, imageName: "")
     private let signUpBtn = CustomButton(text: "회원가입", bgColor: UIColor.white, titleColor: UIColor.black, imageName: "")
@@ -44,6 +74,24 @@ class InitialViewController: UIViewController {
     func setLayout() {
         let basicOffset = 20
         
+        appTitle.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(basicOffset)
+            $0.trailing.equalToSuperview().offset(basicOffset)
+            $0.top.equalToSuperview().inset(150)
+        }
+        
+        appTitle2.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(basicOffset)
+            $0.trailing.equalToSuperview().offset(basicOffset)
+            $0.top.equalTo(appTitle).offset(45)
+        }
+        
+        appLogo.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(170)
+            $0.top.equalToSuperview().offset(310)
+            $0.width.height.equalTo(80)
+        }
+         
         loginBtn.snp.makeConstraints {
             $0.top.equalToSuperview().offset(540)
             $0.centerX.equalToSuperview()
@@ -59,6 +107,9 @@ class InitialViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.addSubview(appTitle)
+        self.view.addSubview(appTitle2)
+        self.view.addSubview(appLogo)
         self.view.addSubview(loginBtn)
         self.view.addSubview(signUpBtn)
         setLayout()
